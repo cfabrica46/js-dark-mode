e29fbd8eec52dcf7b3063013985bef0e3368fae3
Cesar <c.caycho46@gmail.com>
2021-05-28 16:19:06 -0500

    creacion de switch

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-const btnDark = document.querySelector('.div_buttons__btn-dark')
-const btnLight = document.querySelector('.div_buttons__btn-light')
+const btnDark = document.querySelector('.div-buttons__btn-dark')
+const btnLight = document.querySelector('.div-buttons__btn-light')
 
 const body = document.querySelector('.body')
 const title = document.querySelector('.title')
-const div_buttons = document.querySelector('.div_buttons')
+const div_buttons = document.querySelector('.div-buttons')
 
 btnDark.addEventListener('click', () => {
     body.classList.add('body--dark')
     title.classList.add('title--dark')
-    div_buttons.classList.add('div_buttons--dark')
+    div_buttons.classList.add('div-buttons--dark')
 })
 
 btnLight.addEventListener('click', () => {
     body.classList.remove('body--dark')
     title.classList.remove('title--dark')
-    div_buttons.classList.remove('div_buttons--dark')
+    div_buttons.classList.remove('div-buttons--dark')
 })
