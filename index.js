cecafd5f853668fdaf042275b0138614142f1cdd
Cesar <c.caycho46@gmail.com>
2021-05-28 17:18:45 -0500

    switch listo pero no hay iconos ptavidaaaa :')

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const btnDark = document.querySelector('.div-buttons__btn-dark')
 const btnLight = document.querySelector('.div-buttons__btn-light')
 
+const chk = document.getElementById('chk');
+
 const body = document.querySelector('.body')
 const title = document.querySelector('.title')
 const div_buttons = document.querySelector('.div-buttons')
@@ -15,4 +17,10 @@ btnLight.addEventListener('click', () => {
     body.classList.remove('body--dark')
     title.classList.remove('title--dark')
     div_buttons.classList.remove('div-buttons--dark')
+})
+
+chk.addEventListener('change', () => {
+    body.classList.toggle('body--dark')
+    title.classList.toggle('title--dark')
+    div_buttons.classList.toggle('div-buttons--dark')
 })
